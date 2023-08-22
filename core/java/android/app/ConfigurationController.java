de956110adabb5f0f009a916ea97aa04cfdd0d6f
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-08-22 11:11:29 +0530

    Revert "ConfigurationController: Prevent app crash on orientation change"
    This reverts commit b741429e5a0ec0b4c45dc22cddb461a0a6f9593c.

--- core/java/android/app/ConfigurationController.java
+++ core/java/android/app/ConfigurationController.java
@@ -216,7 +216,7 @@ class ConfigurationController {
             final int size = callbacks.size();
             for (int i = 0; i < size; i++) {
                 ComponentCallbacks2 cb = callbacks.get(i);
-                if (!equivalent && cb != null && config != null) {
+                if (!equivalent) {
                     performConfigurationChanged(cb, config);
                 }
             }
